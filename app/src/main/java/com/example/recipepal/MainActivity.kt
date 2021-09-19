d25d334402d098f1036fc03e08eceb1914f59986
akash744 <akashashok12341999@gmail.com>
2021-09-19 17:13:11 +1200

    basics of home screen

--- app/src/main/java/com/example/recipepal/MainActivity.kt
+++ app/src/main/java/com/example/recipepal/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.recipepal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var newNoteIcon: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        newNoteIcon = findViewById(R.id.addNoteIcon)
+        newNoteIcon.setImageResource(R.drawable.profile_male)
+
+
     }
 }
